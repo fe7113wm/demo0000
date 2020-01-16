335c8a17d5524f800f1866aaf84a591992f34ad9
Bao By Xiong <fe7113wm@e2381-as1664962.century.edu>
2020-01-16 11:35:46 -0600

    update

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 for (var i = 0; i < 100; i++) {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'data.txt', false);
+    // var request = new XMLHttpRequest();
+    var request; 
+    if(window.XMLHttpRequest){
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open('GET', 'data.txt');
     
     request.onreadystatechange = function () {
         // document.getElementById('demo').innerHTML = request.responseText;
-        if (request.status === 200 && request.readyState === 4) {
+        if ((request.status === 200) && (request.readyState === 4)) {
             console.log(request);
             document.writeln(request.responseText);
         }
