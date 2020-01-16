5144d7ff5021a697474e7d814eb87e86d7da452e
Bao By Xiong <fe7113wm@e2381-as1664962.century.edu>
2020-01-16 11:21:32 -0600

    minor fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ for (var i = 0; i < 100; i++) {
     var request = new XMLHttpRequest();
     request.open('GET', 'data.txt');
     
-    request.onreadyStateChange = function () {
+    request.onreadystatechange = function () {
         // document.getElementById('demo').innerHTML = request.responseText;
         if (request.status === 200 && request.readyState === 4) {
             console.log(request);
