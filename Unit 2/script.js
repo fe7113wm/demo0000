8044643e3a7a6164f90f7ac104557905377bb49d
Bao By Xiong <fe7113wm@e2381-as1664962.century.edu>
2020-01-28 11:24:58 -0600

    Added Unit 3 Folder

---
+++ Unit 2/script.js
@@ -0,0 +1,26 @@
+
+    // var request = new XMLHttpRequest();
+    var request; 
+    if(window.XMLHttpRequest){
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open('GET', 'data.xml');
+    
+    request.onreadystatechange = function () {
+
+        if ((request.status === 200) && (request.readyState === 4)) {
+            console.log(request.responseXML.getElementsByTagName('artistname')[0].firstChild.nodeValue);
+ 		    var items = request.responseXML.getElementsByTagName('artistname');
+            var output = '<ul>';
+            for (var i = 0; i < items.length; i++) {
+                // output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+                output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+            }
+            output += '</ul>';
+            document.getElementById('update').innerHTML = output;
+        }
+    }
+    request.send();
+    
---
+++ unit 3/scritpt.js
@@ -0,0 +1,27 @@
+
+    // var request = new XMLHttpRequest();
+    var request; 
+    if(window.XMLHttpRequest){
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open('GET', 'data.xml');
+    
+    request.onreadystatechange = function () {
+
+        if ((request.status === 200) && (request.readyState === 4)) {
+            
+           
+             var items = JSON.parse(request.responseText);
+             console.log
+            // var output = '<ul>';
+            // for (var i = 0; i < items.length; i++) {
+            //     // output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+            //     output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+            // }
+            // output += '</ul>';
+            // document.getElementById('update').innerHTML = output;
+        }
+    }
+    request.send();
